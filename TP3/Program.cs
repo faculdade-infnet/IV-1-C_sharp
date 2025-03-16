d9ea99285b260f3cd0ac040a556ce8d338c88d7c
Samuel Hermany <samuelhermany@hotmail.com>
2025-03-16 00:19:04 -0300

    comentado  exercicios no program .cs

--- TP3/Program.cs
+++ TP3/Program.cs
@@ -9,20 +9,20 @@
             var exercicio_01 = new Exercicio_01();
             exercicio_01.Start();
 
-            var exercicio_04 = new Exercicio_04();
-            exercicio_04.Start();
+            //var exercicio_04 = new Exercicio_04();
+            //exercicio_04.Start();
 
-            var exercicio_05 = new Exercicio_05();
-            exercicio_05.Start();
+            //var exercicio_05 = new Exercicio_05();
+            //exercicio_05.Start();
 
-            var exercicio_06 = new Exercicio_06();
-            exercicio_06.Start();
+            //var exercicio_06 = new Exercicio_06();
+            //exercicio_06.Start();
 
-            var exercicio_09 = new Exercicio_09();
-            exercicio_09.Start();
+            //var exercicio_09 = new Exercicio_09();
+            //exercicio_09.Start();
 
-            var exercicio_12 = new Exercicio_12();
-            exercicio_12.Start();
+            //var exercicio_12 = new Exercicio_12();
+            //exercicio_12.Start();
         }
     }
 }
